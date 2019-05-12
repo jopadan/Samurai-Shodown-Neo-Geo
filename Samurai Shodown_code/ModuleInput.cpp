9d219be9b789d8c5db1ee7861b0ae5bc97a2eba5
pgalmor <47526823+PoleteTomaPedete@users.noreply.github.com>
2019-05-12 21:55:27 +0200

    SDL_INIT_JOYSTICK

--- Samurai Shodown_code/ModuleInput.cpp
+++ Samurai Shodown_code/ModuleInput.cpp
@@ -22,6 +22,7 @@ bool ModuleInput::Init()
 	LOG("Init SDL input event system");
 	bool ret = true;
 	SDL_Init(0);
+	SDL_InitSubSystem(SDL_INIT_JOYSTICK);
 
 	if(SDL_InitSubSystem(SDL_INIT_EVENTS) < 0)
 	{
